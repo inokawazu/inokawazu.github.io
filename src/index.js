7fc2f231f82b35e101f2c90ce98f1b8743498d49
Markus-Amano <markus.a.amano@gmail.com>
2021-08-28 21:51:00 +0900

    adjusted styling

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const ARTICLES = [
 function make_articles_table() {
 	let elem = document.getElementById("articles");
 	let table = document.createElement("TABLE");
-	table.className = "w3-table-all w3-centered";
+	table.className = "w3-table-all w3-striped w3-centered w3-hoverable";
 	let header = table.createTHead();
 	
 	for (let i = 1; i<ARTICLES.length; i++) {
@@ -16,6 +16,7 @@ function make_articles_table() {
 		let inspire = ARTICLES[i].inspire;
 
 		let row = header.insertRow(-1);
+		row.className = "w3-hover-light-green"
 		let cell = row.insertCell(-1);
 		let link = document.createElement('a');
 		link.href = "https://inspirehep.net/literature/" + inspire;
