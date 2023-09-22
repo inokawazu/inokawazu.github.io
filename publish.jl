cb9520f30e9f901eee79db91e346ffc79b160745
Markus-Amano <markus.a.amano@gmail.com>
2023-09-22 11:53:32 +0900

    added publish script

---
+++ publish.jl
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S julia --startup-file=no
+
+update_docs_path = joinpath(@__DIR__, "update_docs.sh")
+
+update_docs_cmd = run(`sh $update_docs_path`)
+
+if !success(update_docs_cmd)
+    @error "There was an error with the document update."
+    exit(1)
+end
+
+using Franklin
+publish()
